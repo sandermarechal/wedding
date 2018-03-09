895814d7478adc598e707a33d09e12d6ef25cf2d
Sander Marechal <s.marechal@jejik.com>
2018-03-09 14:27:03 +0100

    Setup base styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,12 @@
 import $ from 'jquery';
 import { Foundation } from 'foundation-sites/js/foundation.core';
+import fontawesome from '@fortawesome/fontawesome';
+import faHeart from '@fortawesome/fontawesome-free-solid/faHeart';
 
 window.$ = $;
 Foundation.addToJquery($);
+
+fontawesome.library.add(faHeart);
 
 $(document).ready(function () {
     $(document).foundation();
