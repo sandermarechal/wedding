b79fc7f68adf90b3272d80bc0fe63adf38bafe94
Sander Marechal <s.marechal@jejik.com>
2018-03-10 18:10:10 +0100

    Add responsive top bars

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,18 @@
 import $ from 'jquery';
-import { Foundation } from 'foundation-sites/js/foundation.core';
+import Foundation from 'foundation-sites';
 import fontawesome from '@fortawesome/fontawesome';
 import faHeart from '@fortawesome/fontawesome-free-solid/faHeart';
 
+// Setup jQuery
 window.$ = $;
+
+// Setup Foundation
 Foundation.addToJquery($);
 
+// Setup FontAwesome
 fontawesome.library.add(faHeart);
 
+// Start
 $(document).ready(function () {
     $(document).foundation();
 });
